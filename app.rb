f138f0b6c37a6cfbfc7abccd208ca015026c2636
Kochapas Santibut <kochapas@outlook.com>
2021-07-20 16:55:45 +0900

    Implement layout for erb.

--- app.rb
+++ app.rb
@@ -1,12 +1,26 @@
-require "sinatra"
-require "sinatra/reloader" if development?
-require "pry-byebug"
-require "better_errors"
+require 'sinatra'
+require 'sinatra/reloader' if development?
+require 'pry-byebug'
+require 'better_errors'
+
+# set :bind, '0.0.0.0'
+
 configure :development do
   use BetterErrors::Middleware
-  BetterErrors.application_root = File.expand_path('..', __FILE__)
+  BetterErrors.application_root = File.expand_path(__dir__)
 end
 
 get '/' do
-  'Hello world!'
+  erb :index
+end
+
+get '/about' do
+  @usernames = %w[ssaunier Papillard]
+  erb :about
+end
+
+get '/team/:username' do
+  # binding.pry
+  puts params[:username]
+  "The username is #{params[:username]}"
 end
